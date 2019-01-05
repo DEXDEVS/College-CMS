b8def141ce0f6ab6feb8a4a9d8d79c1b0a54ecc5
naumanshahid007 <hwhashmi1625@gmail.com>
2019-01-05 18:04:32 +0500

    forms required fields with red star symbol updated

--- backend/views/emp-info/_form.php
+++ backend/views/emp-info/_form.php
@@ -63,43 +63,52 @@ use common\models\Branches;
             <?= $form->field($model, 'emp_marital_status')->dropDownList([ 'Single' => 'Single', 'Married' => 'Married', ], ['prompt' => 'Select Merital Status']) ?>
         </div>
         <div class="col-md-4">
+            <!-- <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 102px; top: 6px"></i> -->
             <?= $form->field($model, 'emp_qualification')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-md-4">
+            <!-- <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 104px; top: 6px"></i> -->
             <?= $form->field($model, 'emp_passing_year')->textInput() ?>
         </div>
     </div>
     <div class="row">
         
         <div class="col-md-4">
+            <!-- <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 113px; top: 6px"></i> -->
             <?= $form->field($model, 'emp_institute_name')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-md-4">
+            <!-- <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 173px; top: 6px"></i> -->
             <?= $form->field($model, 'degree_scan_copy')->fileInput() ?>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4">
+            <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 97px; top: 6px"></i>
             <?= $form->field($model, 'emp_designation_id')->dropDownList(
                     ArrayHelper::map(EmpDesignation::find()->all(),'emp_designation_id','emp_designation'), ['prompt'=>'Select Designation']
                 )?>
         </div>
         <div class="col-md-4">
+            <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 50px; top: 6px"></i>
             <?= $form->field($model, 'emp_type_id')->dropDownList(
                     ArrayHelper::map(EmpType::find()->all(),'emp_type_id','emp_type'), ['prompt'=>'Select Type']
                 )?>
         </div>
         <div class="col-md-4">
+            <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 80px; top: 6px"></i>
             <?= $form->field($model, 'group_by')->dropDownList([ 'Faculty' => 'Faculty', 'Non-Faculty' => 'Non-Faculty', ], ['prompt' => 'Select Group']) ?>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4">
+            <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 107px; top: 6px"></i>
             <?= $form->field($model, 'emp_branch_id')->dropDownList(
                     ArrayHelper::map(Branches::find()->all(),'branch_id','branch_name'), ['prompt'=>'Select Branch']
                 )?>
         </div>
         <div class="col-md-4">
+            <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 58px; top: 6px"></i>
             <?= $form->field($model, 'emp_salary')->textInput() ?>
         </div>
         <div class="col-md-4">
@@ -110,20 +119,24 @@ use common\models\Branches;
 
     <!-- Form of Employee Reference -->
     <div id="referenceshow" style="display: none;">
-        <h3>Emp Reference</h3>
+        <h3 style="color: #337AB7; margin-top: -10px"> Employee Refrence <small> ( Fields with <span style="color: red;">red stars </span>are required )</small> </h3>
         <div class="row">
             <div class="col-md-4">
+                <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 81px; top: 6px"></i>
                 <?= $form->field($empRefModel, 'ref_name')->textInput(['maxlength' => true]) ?>
             </div>
             <div class="col-md-4">
+                <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 118px; top: 6px"></i>
                 <?= $form->field($empRefModel, 'ref_contact_no')->widget(yii\widgets\MaskedInput::class, [ 'mask' => '+99-999-9999999', ]) ?>
             </div>
             <div class="col-md-4">
+                <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 75px; top: 6px"></i>
                 <?= $form->field($empRefModel, 'ref_cnic')->widget(yii\widgets\MaskedInput::class, [ 'mask' => '99999-9999999-9', ]) ?>
             </div>
         </div>
         <div class="row">
             <div class="col-md-4">
+                <i class="fa fa-star" style="font-size: 8px; color: red; position: absolute; left: 123px; top: 6px"></i>
                 <?= $form->field($empRefModel, 'ref_designation')->textInput(['maxlength' => true]) ?>
             </div>
         </div>
