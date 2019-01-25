8808d882fd3ab04785df5991de0ed04e5d85ed0b
anasshafqat <anasshafqat01@gmail.com>
2019-01-25 20:27:14 +0500

    add notice board and events panel in index

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -109,10 +109,155 @@
           <!-- /.info-box -->
         </div>
       </div>
-
       <!-- Message of the day close -->
+      
+      <!-- Notice Row Start -->
+      <div class="row">
+        <!-- Notice Panel Start -->
+        <div class="col-md-7">
+          <!-- Custom Tabs (Pulled to the right) -->
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs pull-right">
+              <li>
+                <a href="#parents" data-toggle="tab">
+                  <i class="fa fa-user-o" style="color: #30BBBB;"></i>
+                   Parents
+                </a>
+              </li>
+              <li>
+                <a href="#employees" data-toggle="tab">
+                  <i class="fa fa-user" style="color: #F39C12;"></i>
+                  Employees
+                </a>
+              </li>
+              <li>
+                <a href="#student" data-toggle="tab">
+                  <i class="fa fa-users" style="color: #00A65A;"></i>
+                  Students
+                </a>
+              </li>
+              <li class="active">
+                <a href="#general" data-toggle="tab">
+                  <i class="fa fa-american-sign-language-interpreting" style="color: #00C0EF;"></i> General
+                </a>
+              </li>
+              <!-- <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                  Dropdown <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                  <li role="presentation" class="divider"></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                </ul>
+              </li> -->
+              <li class="pull-left header"><i class="fa fa-inbox" style="color: #3C8DBC;"></i><span style="color: #3C8DBC;">Notice Board</span></li>
+            </ul>
+            <!-- tab-content start -->
+            <div class="tab-content">
+              <!-- general tab start -->
+              <div class="tab-pane active" id="general">
+                <div class="alert bg-info text-info marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- general tab close -->
+              <!-- ***************** -->
+              <!-- student tab start -->
+              <div class="tab-pane" id="student">
+                <div class="alert bg-success text-success marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- students tab close -->
+              <!-- ***************** -->
+              <!-- employees tab start -->
+              <div class="tab-pane" id="employees">
+                <div class="alert bg-warning text-warning marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- employees tab close -->
+              <!-- ***************** -->
+              <!-- parents tab start -->
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="parents">
+                <div class="alert bg-teal text-warning marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- parents tab close -->
+            </div>
+            <!-- tab-content close -->
+          </div>
+          <!-- nav-tabs-custom -->
+        </div>
+        <!-- Notice Panel CLose -->
 
-      <!-- Main row -->
+        <!-- Notice Panel Start -->
+        <div class="col-md-5">
+          <!-- Custom Tabs (Pulled to the right) -->
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs pull-right">
+              <li>
+                <a href="#upcoming" data-toggle="tab">
+                  <i class="fa fa-recycle" style="color: #5AC594;"></i> 
+                  Upcoming
+                </a>
+              </li>
+              <li class="active">
+                <a href="#today" data-toggle="tab">
+                  <i class="fa fa-clock-o" style="color: #00C0EF;"></i> 
+                  Today
+                </a>
+              </li>
+              <!-- <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                  Dropdown <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                  <li role="presentation" class="divider"></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                </ul>
+              </li> -->
+              <li class="pull-left header"><i class="fa fa-calendar" style="color: #3C8DBC;"></i><span style="color: #3C8DBC;">Events</span></li>
+            </ul>
+            <!-- tab-content start -->
+            <div class="tab-content">
+              <!-- general tab start -->
+              <div class="tab-pane active" id="todays">
+                <div class="alert bg-warning text-warning marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- general tab close -->
+              <!-- ***************** -->
+              <!-- student tab start -->
+              <div class="tab-pane" id="upcoming">
+                <div class="alert bg-warning text-warning marginTop">
+                  <i class="fa fa-warning"></i> 
+                  No record found. 
+                </div>
+              </div>
+              <!-- students tab close -->
+            </div>
+            <!-- tab-content close -->
+          </div>
+          <!-- nav-tabs-custom -->
+        </div>
+        <!-- Notice Panel CLose -->
+      </div>
+      <!-- Notice Row CLose -->
     </section>
     <!-- /.content -->
 </div>
