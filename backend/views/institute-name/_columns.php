6f21c16b20bb22216070a429f4c8e8f833055399
anasshafqat <anasshafqat01@gmail.com>
2019-03-20 10:23:48 +0500

    update institute-name form

--- backend/views/institute-name/_columns.php
+++ backend/views/institute-name/_columns.php
@@ -10,10 +10,10 @@ return [
         'class' => 'kartik\grid\SerialColumn',
         'width' => '30px',
     ],
-        [
-        'class'=>'\kartik\grid\DataColumn',
-        'attribute'=>'Institute_name_id',
-    ],
+    // [
+    //     'class'=>'\kartik\grid\DataColumn',
+    //     'attribute'=>'Institute_name_id',
+    // ],
     [
         'class'=>'\kartik\grid\DataColumn',
         'attribute'=>'Institute_name',
