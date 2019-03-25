65ffbec86b52621d8dc4ac4d8df64675ea09ea50
anasshafqat <anasshafqat01@gmail.com>
2019-03-26 00:44:56 +0500

    modify inquiry graphs views

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -8,32 +8,7 @@ use yii\helpers\Url;
 /* @var $this yii\web\View */
 //$this->title = 'SMART EDUCATION';
 ?>
-<?php  
-    // total inquiries...
-    $query = (new \yii\db\Query())->from('std_inquiry');
-    $countTotal = $query->count('std_inquiry_id');
-    // total male inquiries...
-    $query = (new \yii\db\Query())->from('std_inquiry')->where(['gender'=>'Male']);
-    $countMale = $query->count('std_inquiry_id'); 
-    // total female inquires...
-    $query = (new \yii\db\Query())->from('std_inquiry')->where(['gender'=>'Female']);
-    $countFemale = $query->count('std_inquiry_id');
-    // total registered inquiries...
-    $query = (new \yii\db\Query())->from('std_inquiry')->where(['inquiry_status'=>'Registered']);
-    $countRegistered = $query->count('std_inquiry_id'); 
 
-    $query90 = Yii::$app->db->createCommand("SELECT std_inquiry_id FROM `std_inquiry` WHERE `std_percentage` >= '90%' OR `std_percentage` <= '100%'")->queryAll();
-    $count90 = count($query90);
-    $query80 = Yii::$app->db->createCommand("SELECT std_inquiry_id FROM `std_inquiry` WHERE `std_percentage` >= '80%' AND `std_percentage` <= '89%'")->queryAll();
-    $count80 = count($query80);
-    $query70 = Yii::$app->db->createCommand("SELECT std_inquiry_id FROM `std_inquiry` WHERE `std_percentage` >= '70%' AND `std_percentage` <= '79%'")->queryAll();
-    $count70 = count($query70);
-    $query60 = Yii::$app->db->createCommand("SELECT std_inquiry_id FROM `std_inquiry` WHERE `std_percentage` >= '60%' AND `std_percentage` <= '69%'")->queryAll();
-    $count60 = count($query60);
-    $query50 = Yii::$app->db->createCommand("SELECT std_inquiry_id FROM `std_inquiry` WHERE `std_percentage` >= '50%' AND `std_percentage` <= '59%'")->queryAll();
-    $count50 = count($query50);
-
-?>
 <div class="site-index">
     <!-- Main content -->
     <section class="content">
@@ -148,10 +123,6 @@ use yii\helpers\Url;
           </div>
           <!-- ./col -->
         </div>
-        <div class="row">
-          <div class="col-md-6 col-sm-6" id="container1"></div>
-          <div class="col-md-6 col-sm-6" id="container2"></div>
-      </div><br>
       <?php } 
         else { ?>
         <div class="row">
@@ -817,108 +788,4 @@ Modal::end();
       document.getElementById('sec').innerHTML = secs+' '+am;
   }
   setInterval(clock, 1000)
-</script>
-<script type="text/javascript">
-    Highcharts.chart('container1', {
-        chart: {
-            type: 'pie',
-            options3d: {
-                enabled: true,
-                alpha: 45
-            }
-        },
-        title: {
-            text: '<b>Inquiry - Institute Wise</b>'
-        },
-        subtitle: {
-            text: 'Session 2019 - 2021'
-        },
-        plotOptions: {
-            pie: {
-                innerSize: 60,
-                depth: 100
-            }
-        },
-        series: [{
-            name: 'Number of Students',
-            data: [
-            <?php  
-            // SELECT all Institutes....
-            $queryInstitutes = Yii::$app->db->createCommand("SELECT DISTINCT(previous_institute) FROM std_inquiry")->queryAll();
-            $count = count($queryInstitutes); 
-            for($i = 0; $i <=$count; $i++) {
-                $previousInstitute = $queryInstitutes[$i]['previous_institute'];
-                $queryInquiries = Yii::$app->db->createCommand("SELECT std_inquiry_id,previous_institute FROM std_inquiry WHERE previous_institute = '$previousInstitute'")->queryAll();
-                $count = count($queryInquiries);
-                    echo "['<b>".$queryInquiries[0]['previous_institute']."</b>', ".$count."],";
-                }
-            ?>
-            ]
-        }]
-        
-    });
-// Inquiry - Percentage Wise
-Highcharts.chart('container2', {
-    chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-    },
-    title: {
-        text: '<b>Inquiry - Percentage Wise</b>'
-    },
-    subtitle: {
-            text: 'Session 2019 - 2021'
-        },
-    // tooltip: {
-    //     pointFormat: '{series.name}: <b>{point.percentage:.1f}</b>'
-    // },
-    plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.y:1f}',
-                style: {
-                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                }
-            }
-        }
-    },
-    series: [{
-        name: 'Students',
-        colorByPoint: true,
-        data: [
-            {
-             "name": "<b>90% to 100%</b>",
-                "y": <?php echo $count90; ?>,              
-              sliced: true,
-              selected: true
-
-            },
-            {
-                "name": "<b>80% to 89%</b>",
-                "y": <?php echo $count80; ?>,
-                "drilldown": "Firefox"
-            },
-            {
-                "name": "<b>70% to 79%</b>",
-                "y": <?php echo $count70; ?>,
-                "drilldown": "Internet Explorer"
-            },
-            {
-                "name": "<b>60% to 69%</b>",
-                "y": <?php echo $count60; ?>,
-                "drilldown": null
-            },
-            {
-                "name": "<b>50% to 59%</b>",
-                "y": <?php echo $count50; ?>,
-                "drilldown": null
-            }
-        ]
-    }]
-});
 </script>
